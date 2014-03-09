e180d26f82160f365430af2ca04575bf0854439a
chuangxiaomoo <chuangxiaomoo@gmail.com>
2014-03-09 23:54:13 +0800

    345.sql

--- stk/sql/345.sql
+++ stk/sql/345.sql
@@ -24,7 +24,7 @@ CREATE PROCEDURE sp_avrg(a_tbl CHAR(20), a_x CHAR(20), a_period INT) tag_avrg:BE
 END tag_avrg//
 
 DROP PROCEDURE IF EXISTS sp_ma//
-CREATE PROCEDURE sp_ma(a_tbl CHAR(20), a_code INT(6) ZEROFILL) tag_ma:BEGIN
+CREATE PROCEDURE sp_ma(a_code INT(6) ZEROFILL) tag_ma:BEGIN
     DROP TABLE IF EXISTS ma;
     CREATE TABLE ma (
         id          INT(6) PRIMARY key AUTO_INCREMENT NOT NULL,
@@ -39,10 +39,6 @@ CREATE PROCEDURE sp_ma(a_tbl CHAR(20), a_code INT(6) ZEROFILL) tag_ma:BEGIN
         ma6         DECIMAL(6,3) NOT NULL DEFAULT 0,
         INDEX(date)
     );
-
-    -- SET @sqls=concat('INSERT INTO ma(code,date,close) SELECT code,date,close FROM ',
-    --     a_tbl, ' WHERE code=', a_code);
-    -- PREPARE stmt from @sqls; EXECUTE stmt;
 
     INSERT INTO ma(code,date,close)
         SELECT code,date,close FROM day WHERE code=a_code and date<=@END;
@@ -59,8 +55,48 @@ CREATE PROCEDURE sp_ma(a_tbl CHAR(20), a_code INT(6) ZEROFILL) tag_ma:BEGIN
  -- call sp_avrg('ma', 'ma6', 144);
 END tag_ma//
 
-SET @END = '2014-3-10';
-call sp_ma('day', 002708);
-
 -- 将最后30天记录INSERT到表 tbl_ma_recent 以做快速索引
+
+DROP PROCEDURE IF EXISTS sp_visit_tbl//
+CREATE PROCEDURE sp_visit_tbl(a_tbl CHAR(20), a_type INT) tag_visit:BEGIN
+    DECLARE v_code  INT(6) ZEROFILL;
+    DECLARE v_id    INT DEFAULT 1; 
+    DECLARE v_len   INT; /* CURSOR and HANDLER declare in end of declaration */
+    
+    DROP TABLE IF EXISTS codes;
+    CREATE TABLE codes (
+        id          INT PRIMARY key AUTO_INCREMENT NOT NULL,
+        code        INT(6) ZEROFILL NOT NULL DEFAULT 0
+    );
+    DROP TABLE IF EXISTS tbl_visit_ma; -- for visit output
+    CREATE TABLE tbl_visit_ma(
+        id          INT(6) PRIMARY key AUTO_INCREMENT NOT NULL,
+        code        INT(6) ZEROFILL NOT NULL DEFAULT 0,
+        date        date NOT NULL,
+        close       DECIMAL(6,2) NOT NULL DEFAULT 0,
+        ma1         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        ma2         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        ma3         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        ma4         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        ma5         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        ma6         DECIMAL(6,3) NOT NULL DEFAULT 0,
+        INDEX(code,date)
+    );
+
+    SET @cond=' ORDER by code';
+    SET @sqls=concat('INSERT INTO codes(code) SELECT code FROM ', a_tbl, @cond);
+    PREPARE stmt from @sqls; EXECUTE stmt;
+    SELECT max(id)   FROM codes INTO v_len;
+    SELECT max(date) FROM day WHERE code=900001 INTO @v_maxdate;
+
+    WHILE v_id <= v_len DO
+        SELECT code FROM codes WHERE id=(v_id) INTO v_code;
+        call sp_ma(v_code);
+        -- 读取后20条记录
+        SET v_id = v_id + 1;
+    END WHILE;
+END tag_visit //
+
+SET @END = '2014-3-10';
+call sp_ma(002708);
 
