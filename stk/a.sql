313bd7fa1ab4df8dc813a8fb2cbebdf54e2c65a7
chuangxiaomoo <chuangxiaomoo@gmail.com>
2015-08-15 00:33:34 +0800

    ma2015

---
+++ stk/a.sql
@@ -0,0 +1,84 @@
+
+    DROP TABLE IF EXISTS dorat2;
+    CREATE TABLE dorat2 LIKE dorat;
+    
+
+        SELECT '2015-08-14', '15:04:10';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='15:04:10';
+        
+
+        SELECT '2015-08-14', '14:44:54';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='14:44:54';
+        
+
+        SELECT '2015-08-14', '14:29:56';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='14:29:56';
+        
+
+        SELECT '2015-08-14', '14:14:52';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='14:14:52';
+        
+
+        SELECT '2015-08-14', '13:59:58';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='13:59:58';
+        
+
+        SELECT '2015-08-14', '13:44:59';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='13:44:59';
+        
+
+        SELECT '2015-08-14', '13:29:58';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='13:29:58';
+        
+
+        SELECT '2015-08-14', '13:14:46';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='13:14:46';
+        
+
+        SELECT '2015-08-14', '12:59:50';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='12:59:50';
+        
+
+        SELECT '2015-08-14', '11:14:52';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='11:14:52';
+        
+
+        SELECT '2015-08-14', '11:00:11';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='11:00:11';
+        
+
+        SELECT '2015-08-14', '10:44:59';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='10:44:59';
+        
+
+        SELECT '2015-08-14', '10:29:54';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='10:29:54';
+        
+
+        SELECT '2015-08-14', '10:15:02';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='10:15:02';
+        
+
+        SELECT '2015-08-14', '09:59:56';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='09:59:56';
+        
+
+        SELECT '2015-08-14', '09:44:55';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-14' and time='09:44:55';
+        
+
+        SELECT '2015-08-13', '15:04:02';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-13' and time='15:04:02';
+        
+
+        SELECT '2015-08-13', '14:44:56';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-13' and time='14:44:56';
+        
+
+        SELECT '2015-08-13', '14:29:50';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-13' and time='14:29:50';
+        
+
+        SELECT '2015-08-13', '14:14:52';
+        INSERT INTO dorat2 SELECT * FROM dorat WHERE date='2015-08-13' and time='14:14:52';
+        
