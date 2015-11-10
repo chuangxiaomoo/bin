fd0c68e5faf3fd4f9677e9a9d75ff55192538a91
chuangxiaomoo <chuangxiaomoo@gmail.com>
2015-11-10 23:42:40 +0800

    wolf.dde

--- stk/sql/t.sql
+++ stk/sql/t.sql
@@ -17,3 +17,27 @@ END //
 
 
 call sp_cp_tbl(34);
+
+-- 
+
+    CREATE TABLE IF NOT EXISTS mat_dde (
+        id          INT PRIMARY key AUTO_INCREMENT NOT NULL,
+        date        date NOT NULL DEFAULT 0,
+        time        time NOT NULL, 
+        code        INT(6) ZEROFILL NOT NULL DEFAULT 0,
+        name        CHAR(16),
+        pcnt        DECIMAL(6,2) NOT NULL DEFAULT 0, -- dec(p-n)/sum(p+n)
+        pbuy        DECIMAL(6,2) NOT NULL DEFAULT 0,
+        nbuy        DECIMAL(6,2) NOT NULL DEFAULT 0,
+        psell       DECIMAL(6,2) NOT NULL DEFAULT 0,
+        nsell       DECIMAL(6,2) NOT NULL DEFAULT 0,
+        rise        DECIMAL(6,2) NOT NULL DEFAULT 0,
+        trade       DECIMAL(6,2) NOT NULL DEFAULT 0,
+        speed       DECIMAL(6,2) NOT NULL DEFAULT 0,
+        shou        DECIMAL(8,2) NOT NULL DEFAULT 0,
+        tov         DECIMAL(8,2) NOT NULL DEFAULT 0,
+        amount      DECIMAL(8,2) NOT NULL DEFAULT 0,
+        nmc         DECIMAL(8,2) NOT NULL DEFAULT 0,
+        INDEX(date,time,code)
+    );
+
