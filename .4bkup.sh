877aed7a61b18c9cf72c25262b708670d5801632
chuangxiaomoo <chuangxiaomoo@gmail.com>
2024-09-30 17:50:11 +0800

    cid

--- .4bkup.sh
+++ .4bkup.sh
@@ -1,2 +1,8 @@
 #! /bin/sh
-rsync -avz --delete --filter='P .git' test4g@121.40.104.39:/home/test4g/ $@
+
+# ~/bin/4bkup.rsync is old
+
+. ~/gitee/social/priv/test4g.rc
+
+sshpass -p ${t4g_pass} rsync -avz --delete --filter='P .git' test4g@121.40.104.39:/home/test4g/www/tool ${1:-/home/test4g/www/tool}
+
